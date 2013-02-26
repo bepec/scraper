bbebaf4b0188ebdf02b272b5db118f6d08d42b27
Alex Vedmidskiy <gagaga1024@gmail.com>
2013-02-26 14:07:02 +0200

    + spider ignores already crawled items + added timestamp column to 'offers' table

--- slando_apt_od/slando_apt_od/pipelines.py
+++ slando_apt_od/slando_apt_od/pipelines.py
@@ -3,6 +3,8 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/topics/item-pipeline.html
 import sqlite3
+import time, datetime
+from scrapy import log
 
 class WriteDbPipeline(object):
 
@@ -19,17 +21,17 @@ class WriteDbPipeline(object):
 			       ,link text
 			       ,price float
 			       ,desc text
-			       ,addr text)''')
-	self.cursor.execute("insert into offers(id, title) values('abc', 'a big cock')")
+			       ,addr text
+			       ,added int)''')
+	    log.msg("Created table 'offers' in db")
 
     def process_item(self, item, spider):
-	values = (item['id'], item['num'], item['title'], item['link'], item['price'], item['desc'], item['addr'])
-	print "executing INSERT statement"
+	timestamp = time.mktime(datetime.datetime.now().timetuple())
+	values = (item['id'], item['num'], item['title'], item['link'], item['price'], item['desc'], item['addr'], timestamp)
 	cursor = self.conn.cursor()
-	# print cursor.execute("INSERT INTO offers VALUES(?, ?, ?, ?, ?, ?, ?)", values)
-	print self.conn.execute("INSERT INTO offers VALUES(?, ?, ?, ?, ?, ?, ?)", values)
-	# print cursor.execute("INSERT INTO offers(id, title) VALUES(?, ?)", (item['id'], item['title']))
+	self.conn.execute("INSERT INTO offers VALUES(?, ?, ?, ?, ?, ?, ?, ?)", values)
 	self.conn.commit()
+	log.msg("Inserted item with ID={0} into db".format(item['id']))
         return item
 
     def table_exists(self):
--- slando_apt_od/slando_apt_od/spiders/slando_spider.py
+++ slando_apt_od/slando_apt_od/spiders/slando_spider.py
@@ -3,7 +3,9 @@ import re
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.selector import HtmlXPathSelector
+from scrapy import log
 from slando_apt_od.items import SlandoAptOdItem
+import sqlite3
 
 class SlandoSpider(CrawlSpider):
     name = "slando"
@@ -13,49 +15,39 @@ class SlandoSpider(CrawlSpider):
     ]
 
     rules = (
-	Rule(SgmlLinkExtractor(allow=('obyavlenie',)), callback='parse_item'),
+	Rule(SgmlLinkExtractor(allow=('obyavlenie',)), callback='parse_item', process_links='process_links'),
     )
 
+    def process_links(self, links):
+	return self.links_not_in_db(links)
+
     def parse_item(self, response):
-#	self.log('parsing %s' % response.url)
 	hxs = HtmlXPathSelector(response)
 	content = hxs.select('//div[@class="content"]')
 	header = content.select('div[@class="margintop20 clr"]')
 	item = SlandoAptOdItem()
 	item['link'] = response.url 
 	item['id'] = re.search(r'-ID(.{5})\.html', response.url).group(1)
-	item['num'] = header.select('p/small/span/span/text()').extract()[0].strip()
+	item['num'] = header.select('p/small/span/span/span/text()').extract()[0].strip()
 	item['title'] = header.select('h1/text()').extract()[0].strip()
-	print 'item'
-	# print item
 	item['desc'] = content.select('div[2]/div[1]/div[2]/p/text()').extract()[0].strip()
 	item['price'] = content.select('div[2]/div[2]/div[1]/div[1]/div[1]/strong/text()').extract()[0].strip()
 	item['addr'] = content.select('div[2]/div[2]/div[1]/div[4]/table//address/p[2]/text()').extract()[0].strip()
 	return item
 
+    def links_not_in_db(self, links):
+	connection = sqlite3.connect('/tmp/slando.db')	
+	cursor = connection.cursor()
+	id_list = set()
+	filtered_links = list()
+	cursor.execute('select id from offers')
+	for row in cursor:
+	    id_list.add(row[0])
+	for link in links:
+	    id = re.search(r'-ID(.{5})\.html', link.url).group(1)
+	    if not id in id_list: 
+		filtered_links.append(link)
+	    else:
+		log.msg("Item with ID={0} is already in db, skipping.".format(id))
+	return filtered_links
 
-class SandoSpider:
-    name = "slando"
-    allowed_domains = ["slando.ua"]
-    start_urls = [
-	    "http://odessa.od.slando.ua/nedvizhimost/?all_categories=1&search[filter_float_price%3Afrom]=2400&search[filter_float_price%3Ato]=3200",
-	    # "http://odessa.od.slando.ua/nedvizhimost/?all_categories=1&search[filter_float_price%3Afrom]=2400&search[filter_float_price%3Ato]=3200&page=2",
-	    # "http://odessa.od.slando.ua/nedvizhimost/?all_categories=1&search[filter_float_price%3Afrom]=2400&search[filter_float_price%3Ato]=3200&page=3" 
-    ]
-
-    def parse(self, response):
-	hxs = HtmlXPathSelector(response)
-	offers = hxs.select('//table[@id="offers_table"]/tbody/tr/td/table/tbody/tr')
-	print "offers found: {0}".format(len(offers))
-	items = []
-	for offer in offers:
-	    header = offer.select('td/h3')
-	    item = SlandoAptOdItem()
-	    item['link'] = header.select('a/@href').extract()[0].strip()
-	    item['id'] = re.search(r'-ID(.{5})\.html', item['link']).group(1)
-	    item['title'] = header.select('a/span/text()').extract()[0].strip()
-	    item['price'] = offer.select('td/div/p/strong/text()').extract()[0].strip()
-	    items.append(item)
-	return items
-	    # string = title + ': ' + price
-	    # print string.encode('utf-8')
